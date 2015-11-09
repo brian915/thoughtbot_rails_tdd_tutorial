a229851819a54732bcde8dc815a1dc26e18be957
brian915 <brian@brianrunk.net>
2015-11-08 19:25:13 -0500

    added smoke test for todos app homepage (index)

--- todos/spec/features/user_visits_homepage_spec.rb
+++ todos/spec/features/user_visits_homepage_spec.rb
@@ -1,9 +1,8 @@
 require "rails_helper"
 
-feature "user visits homepage"  do
+feature "user visits homepage"  do  
   scenario  "successfully" do 
-    visit root_path
-
-    expect(page).to have_css 'h1', text: 'Todos'
+    visit root_path 
+    expect(page).to have_css 'h1', text: 'TODOS' 
   end
 end
