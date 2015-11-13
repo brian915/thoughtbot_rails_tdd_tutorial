18a3eb371ba11abc7b29b421205d054644bc206b
brian915 <brian@brianrunk.net>
2015-11-13 06:33:45 -0500

    refactored tests and completions controller

--- todos/spec/features/user_completes_todo_spec.rb
+++ todos/spec/features/user_completes_todo_spec.rb
@@ -3,17 +3,14 @@ require "rails_helper"
 feature "User completes todo" do
   scenario "successfully" do
     sign_in
-   
-    click_on "Add a new todo"
-    fill_in "Title", with: "Buy milk"
-    click_on "Submit"
 
-    expect(page).to have_css '.todos li', text: "Buy milk"
+    create_todo "Buy milk"
+    expect(page).to display_todo "Buy milk"
 
     click_on "Mark Complete"
+    expect(page).to display_completed_todo "Buy milk"
 
-    expect(page).to have_css '.todos li.completed', text: "Buy milk"
 
-    save_and_open_page
+#    save_and_open_page
   end
 end
--- todos/spec/features/user_creates_todo_spec.rb
+++ todos/spec/features/user_creates_todo_spec.rb
@@ -3,11 +3,7 @@ require "rails_helper"
 feature "User creates Todo" do
   scenario "successfully" do
     sign_in
-
-    click_on "Add a new todo"
-    fill_in "Title", with: "Buy milk"
-    click_on "Submit"
-#    save_and_open_page
-    expect(page).to have_css '.todos li', text: "Buy milk"
+    create_todo "Buy milk"
+    expect(page).to display_todo "Buy milk"
   end
 end
